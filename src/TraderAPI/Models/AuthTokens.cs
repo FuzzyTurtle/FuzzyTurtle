633d369c73fdb1b2624a1e3ab56acd8efc385a8e
Christopher Leighton <christopher7694@gmail.com>
2025-06-13 00:02:01 -0700

    initial changes - stopping point to save remotely

---
+++ src/TraderAPI/Models/AuthTokens.cs
@@ -0,0 +1,12 @@
+﻿namespace TraderAPI.Models;
+
+public sealed record AuthTokens(
+    string Code,
+    string Session,
+    string TokenType,
+    string Scope,
+    DateTime Expiration,
+    string RefreshToken,
+    string AccessToken,
+    string IdToken);
+
---
+++ src/TraderAPI/Models/ClientOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace TraderAPI.Models;
+
+public sealed record ClientOptions(
+    string AuthTokensFileLocation,
+    string DeveloperAppKey,
+    string DeveloperAppSecret,
+    string DeveloperAppCallbackUrl,
+    string TradingAccountUsername,
+    string TradingAccountPassword);
