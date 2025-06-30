354b1e5e317af2f232c43024603ebebc3f6a8685
Christopher Leighton <christopher7694@gmail.com>
2025-06-29 21:44:36 -0700

    Add rate limiting and refactor candlestick data handling
    - Updated `Directory.Packages.props` and `CandlestickDownloader.csproj` to include `System.Threading.RateLimiting`.
    - Refactored `CandlestickDownloaderOptions.cs` to simplify properties, replacing multiple required fields with a list of symbols and a data folder path.
    - Enhanced `Program.cs` to implement a rate limiter, add logging, and manage candlestick data retrieval in a loop.
    - Introduced `Candlestick` and `CandlestickHistory` classes to represent individual candlestick data and collections, respectively.

---
+++ src/CandlestickDownloader/Candlestick.cs
@@ -0,0 +1,11 @@
+﻿namespace CandlestickDownloader;
+
+public class Candlestick
+{
+    public required double Open { get; init; }
+    public required double High { get; init; }
+    public required double Low { get; init; }
+    public required double Close { get; init; }
+    public required long Volume { get; init; }
+    public required long DateTime { get; init; }
+}
--- src/CandlestickDownloader/CandlestickDownloaderOptions.cs
+++ src/CandlestickDownloader/CandlestickDownloaderOptions.cs
@@ -1,13 +1,6 @@
 ﻿namespace CandlestickDownloader;
 public sealed record class CandlestickDownloaderOptions
 {
-    public required string Symbols { get; init; }
-    public required string PeriodType { get; init; }
-    public required int Period { get; init; }
-    public required string FrequencyType { get; init; }
-    public required int Frequency { get; init; }
-    public required long StartDate { get; init; }
-    public required long EndDate { get; init; }
-    public required bool NeedExtendedData { get; init; }
-    public required bool NeedPreviousClose { get; init; }
+    public required List<string> Symbols { get; init; }
+    public required string DataFolder { get; init; }
 }
